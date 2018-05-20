c9d672fafa4ca787310797a653467c46ec05030a
muzyka.viacheslav <muzyka.viacheslav@gmail.com>
2018-05-20 21:22:21 +0300

    refactor, finish up with history feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var amount_of_rectangles = 5;
 var rects = [];
 var outputRects = [];
+const localStorageOutputRectanglesPrefix = "output_result_rectangles_with_index_";
 document.getElementById("rectangles-amount").value = amount_of_rectangles;
 
 function Rectangle(x, y, width, height) {
@@ -14,6 +15,7 @@ function getLastIndexInLocalStorage() {
     return _.max(returnIndexesInLS());
 }
 
+// return all indexes which are stored in Local Storage
 function returnIndexesInLS() {
     const values = [];
     for (var i = 0, len = localStorage.length; i < len; ++i) {
@@ -22,14 +24,15 @@ function returnIndexesInLS() {
     return _.without(values, undefined, null, NaN);
 }
 
+// generate markup for history popup
 function showHistory() {
-    const availableEntries = returnIndexesInLS();
+    const availableEntries = _.orderBy(returnIndexesInLS());
     document.getElementById("history-rows").innerHTML = "";
     if (!availableEntries || availableEntries.length === 0) {
         fillBodyOfNoEntries();
     } else {
         var table = document.createElement("table");
-        table.className = "table table-hover";
+        table.className = "table";
         table.style.marginBottom = "0";
         availableEntries.forEach(function (index) {
             var row = document.createElement("tr");
@@ -42,16 +45,18 @@ function showHistory() {
                 JSON.parse(localStorage.getItem(ev.srcElement.innerHTML)).forEach(function (str) {
                     getParamsFromHumanString(str);
                 });
-                console.log(rects);
                 drawRects('canvasInput', rects);
+                buildOutputRectangles(true);
+                $("#historyModal").modal('hide');
             });
             table.appendChild(row);
         });
+        document.getElementById("history-rows").appendChild(table);
     }
-    document.getElementById("history-rows").appendChild(table);
     $("#historyModal").modal('show');
 }
 
+// function for drawing rectangles on canvas
 function drawRects(canvasId, rects) {
     var c = document.getElementById(canvasId);
     c.height = c.offsetHeight;
@@ -62,16 +67,17 @@ function drawRects(canvasId, rects) {
         ctx.rect(rect.x, rect.y, rect.width, rect.height);
         ctx.stroke();
     });
-    buildOutputRectangles();
 }
 
 function getParamsFromHumanString(str) {
-    console.log();
-    const newRectangle = new Rectangle(str.split("=")[1].split(",")[0],
-        str.split("=")[2].split(",")[0],
-        str.split("=")[3].split(",")[0],
-        str.split("=")[4].split(",")[0]);
-    rects.push(newRectangle);
+    const newX = parseFloat(str.split("=")[1].split(",")[0]);
+    const newY = parseFloat(str.split("=")[2].split(",")[0]);
+    const newWidth = parseFloat(str.split("=")[3].split(",")[0]);
+    const newHeight = parseFloat(str.split("=")[4].split(",")[0]);
+    if (!!newX && !!newY && !!newWidth && !!newHeight) {
+        const newRectangle = new Rectangle(newX, newY, newWidth, newHeight);
+        rects.push(newRectangle);
+    }
 }
 
 function fillBodyOfNoEntries() {
@@ -87,25 +93,20 @@ function saveRectsInLocalStorage(array, source, lastIndex) {
         result_array.push(`Rectangle with parameters: x=${rect.x}, y=${rect.y}, width=${rect.width}, height=${rect.height}`);
     });
     if (!lastIndex) {
-        lastIndex = source === 'input' ? "1" : 'output_result_rectangles_with_index_1';
+        lastIndex = source === 'input' ? 1 : `${localStorageOutputRectanglesPrefix}1`;
     } else {
-        lastIndex = source === 'input' ? lastIndex + 1 : `output_result_rectangles_with_index_${lastIndex + 1}`;
+        lastIndex = source === 'input' ? lastIndex + 1 : `${localStorageOutputRectanglesPrefix}${lastIndex + 1}`;
     }
     localStorage.setItem(lastIndex.toString(), JSON.stringify(result_array));
 }
 
+// function for building random rectangles
 function buildRandomRectangles() {
     rects.length = 0;
-    outputRects.length = 0;
     var c = document.getElementById("canvasInput");
-    c.height = c.offsetHeight;
-    c.width = c.offsetWidth;
-    var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, c.offsetWidth, c.offsetHeight);
     var nextRectLeftPosition = 0;
     var availableSpaceInCanvas = c.offsetWidth;
     var heightOfCanvas = c.offsetHeight;
-    ctx.lineWidth = "1";
     for (var i = 0; i < amount_of_rectangles; i++) {
         const rectWidth = returnRandomIndex(1, availableSpaceInCanvas / (amount_of_rectangles / 3));
         const rectHeight = returnRandomIndex(1, heightOfCanvas);
@@ -114,32 +115,25 @@ function buildRandomRectangles() {
             rectWidth,
             rectHeight);
         rects.push(newRectangle);
-        ctx.rect(newRectangle.x, newRectangle.y, newRectangle.width, newRectangle.height);
-        ctx.stroke();
         availableSpaceInCanvas = availableSpaceInCanvas - newRectangle.width;
         nextRectLeftPosition = nextRectLeftPosition + newRectangle.width;
     }
+    drawRects("canvasInput", rects);
     buildOutputRectangles();
 }
 
-function buildOutputRectangles() {
-    var c = document.getElementById("canvasOutput");
-    c.height = c.offsetHeight;
-    c.width = c.offsetWidth;
-    var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, c.offsetWidth, c.offsetHeight);
-    ctx.lineWidth = "1";
-
+function buildOutputRectangles(justRender) {
+    outputRects.length = 0;
     returnRectangleWithCurrentMinHeight(rects, 0);
     const lastIndex = getLastIndexInLocalStorage();
-    saveRectsInLocalStorage(rects, 'input', lastIndex);
-    saveRectsInLocalStorage(outputRects, 'output', lastIndex);
-    _.each(outputRects, function (rect) {
-        ctx.rect(rect.x, rect.y, rect.width, rect.height);
-        ctx.stroke();
-    });
+    if (!justRender) {
+        saveRectsInLocalStorage(rects, 'input', lastIndex);
+        saveRectsInLocalStorage(outputRects, 'output', lastIndex);
+    }
+    drawRects('canvasOutput', outputRects);
 }
 
+// function to render rectangle with the lowest high in area
 function returnRectangleWithCurrentMinHeight(viewedRects, minY) {
     if (viewedRects.length === 0) return;
     if (viewedRects.length === 1) {
@@ -160,12 +154,14 @@ function returnRectangleWithCurrentMinHeight(viewedRects, minY) {
     }
 }
 
+// generate random index
 function returnRandomIndex(min, max) {
     var rand = min + Math.random() * (max + 1 - min);
     rand = Math.floor(rand);
     return rand;
 }
 
+// function for checking amount of rectangles
 function checkIfValidAmountOfRectangles(element) {
     const value = element.value;
     if (value < 3) {
