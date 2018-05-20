f779edb00c14ae6c29a745af09332f2d91efe275
muzyka.viacheslav <muzyka.viacheslav@gmail.com>
2018-05-20 13:31:03 +0300

    add localStorage flow, history feature

--- app.js
+++ app.js
@@ -10,6 +10,90 @@ function Rectangle(x, y, width, height) {
     this.height = height;
 }
 
+function getLastIndexInLocalStorage() {
+    return _.max(returnIndexesInLS());
+}
+
+function returnIndexesInLS() {
+    const values = [];
+    for (var i = 0, len = localStorage.length; i < len; ++i) {
+        values.push(parseFloat(localStorage.key(i)));
+    }
+    return _.without(values, undefined, null, NaN);
+}
+
+function showHistory() {
+    const availableEntries = returnIndexesInLS();
+    document.getElementById("history-rows").innerHTML = "";
+    if (!availableEntries || availableEntries.length === 0) {
+        fillBodyOfNoEntries();
+    } else {
+        var table = document.createElement("table");
+        table.className = "table table-hover";
+        table.style.marginBottom = "0";
+        availableEntries.forEach(function (index) {
+            var row = document.createElement("tr");
+            var td = document.createElement("td");
+            td.innerHTML = index.toString();
+            row.appendChild(td);
+            row.setAttribute('data-index', index);
+            row.addEventListener("click", function (ev) {
+                rects.length = 0;
+                JSON.parse(localStorage.getItem(ev.srcElement.innerHTML)).forEach(function (str) {
+                    getParamsFromHumanString(str);
+                });
+                console.log(rects);
+                drawRects('canvasInput', rects);
+            });
+            table.appendChild(row);
+        });
+    }
+    document.getElementById("history-rows").appendChild(table);
+    $("#historyModal").modal('show');
+}
+
+function drawRects(canvasId, rects) {
+    var c = document.getElementById(canvasId);
+    c.height = c.offsetHeight;
+    c.width = c.offsetWidth;
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.offsetWidth, c.offsetHeight);
+    rects.forEach(function (rect) {
+        ctx.rect(rect.x, rect.y, rect.width, rect.height);
+        ctx.stroke();
+    });
+    buildOutputRectangles();
+}
+
+function getParamsFromHumanString(str) {
+    console.log();
+    const newRectangle = new Rectangle(str.split("=")[1].split(",")[0],
+        str.split("=")[2].split(",")[0],
+        str.split("=")[3].split(",")[0],
+        str.split("=")[4].split(",")[0]);
+    rects.push(newRectangle);
+}
+
+function fillBodyOfNoEntries() {
+    var div = document.createElement("div");
+    div.className = "no-entries";
+    div.innerHTML = 'NO ENTRIES';
+    document.getElementById("history-rows").appendChild(div);
+}
+
+function saveRectsInLocalStorage(array, source, lastIndex) {
+    var result_array = [];
+    array.forEach(function (rect) {
+        result_array.push(`Rectangle with parameters: x=${rect.x}, y=${rect.y}, width=${rect.width}, height=${rect.height}`);
+    });
+    if (!lastIndex) {
+        lastIndex = source === 'input' ? "1" : 'output_result_rectangles_with_index_1';
+    } else {
+        lastIndex = source === 'input' ? lastIndex + 1 : `output_result_rectangles_with_index_${lastIndex + 1}`;
+    }
+    localStorage.setItem(lastIndex.toString(), JSON.stringify(result_array));
+}
+
 function buildRandomRectangles() {
     rects.length = 0;
     outputRects.length = 0;
@@ -47,9 +131,12 @@ function buildOutputRectangles() {
     ctx.lineWidth = "1";
 
     returnRectangleWithCurrentMinHeight(rects, 0);
+    const lastIndex = getLastIndexInLocalStorage();
+    saveRectsInLocalStorage(rects, 'input', lastIndex);
+    saveRectsInLocalStorage(outputRects, 'output', lastIndex);
     _.each(outputRects, function (rect) {
-       ctx.rect(rect.x, rect.y, rect.width, rect.height);
-       ctx.stroke();
+        ctx.rect(rect.x, rect.y, rect.width, rect.height);
+        ctx.stroke();
     });
 }
 
